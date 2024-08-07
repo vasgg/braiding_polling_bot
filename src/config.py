6067c0c97362ed553fc3548cb10e86c803fe1eee
vasgg <=>
2024-08-08 03:55:51 +0400

    some config

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
+
 from logging.handlers import RotatingFileHandler
 import sys
 
-from pydantic import BaseModel, SecretStr
+from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from enums import Stage
@@ -9,31 +10,19 @@ from enums import Stage
 
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
+    NGROK_URL: SecretStr
+    NGROK_USER: SecretStr
+    NGROK_PASS: SecretStr
     ADMIN: int
-    DBPASS: SecretStr
-    DBNAME: str
-    DBUSER: str
-    DBHOST: str
-    echo: bool = False
-    pool_size: int = 50
-    max_overflow: int = 10
-    stage: Stage
+    DB_NAME: str
+    STAGE: Stage
+    db_echo: bool = False
 
     @property
-    def postgres_db_url(self) -> str:
-        return f'postgresql+asyncpg://{self.DBUSER}:{self.DBPASS.get_secret_value()}@{self.DBHOST}/{self.DBNAME}'
+    def aiosqlite_db_url(self) -> str:
+        return f'sqlite+aiosqlite:///{self.DB_NAME}.db'
 
-    naming_convention: dict[str, str] = {
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
-    }
-
-    model_config = SettingsConfigDict(env_file='.env',
-                                      env_file_encoding='utf-8',
-                                      case_sensitive=False)
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', case_sensitive=False, extra='allow')
 
 
 settings = Settings()
@@ -45,12 +34,12 @@ def get_logging_config(app_name: str):
         "disable_existing_loggers": False,
         "formatters": {
             "main": {
-                "format": "%(asctime)s.%(msecs)03d|%(levelname)s|%(module)s|%(funcName)s: %(message)s",
-                "datefmt": "%d.%m.%Y|%H:%M:%S%z",
+                "format": "%(asctime)s.%(msecs)03d [%(levelname)8s] [%(module)s:%(funcName)s:%(lineno)d] %(message)s",
+                "datefmt": "%d.%m.%Y %H:%M:%S%z",
             },
             "errors": {
-                "format": "%(asctime)s.%(msecs)03d|%(levelname)s|%(module)s|%(funcName)s: %(message)s",
-                "datefmt": "%d.%m.%Y|%H:%M:%S%z",
+                "format": "%(asctime)s.%(msecs)03d [%(levelname)8s] [%(module)s:%(funcName)s:%(lineno)d] %(message)s",
+                "datefmt": "%d.%m.%Y %H:%M:%S%z",
             },
         },
         "handlers": {
@@ -73,6 +62,7 @@ def get_logging_config(app_name: str):
                 "filename": f"logs/{app_name}_log.log",
                 "maxBytes": 500000,
                 "backupCount": 3,
+                "encoding": "utf-8",
             },
         },
         "loggers": {
