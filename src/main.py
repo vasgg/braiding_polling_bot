164b03f2cd69a48040db3fa4b81cb2561185af5c
vasgg <=>
2024-08-08 03:56:10 +0400

    refactor

--- src/main.py
+++ src/main.py
@@ -15,7 +15,6 @@ from bot.middlewares.auth_middleware import AuthMiddleware
 from bot.middlewares.session_middleware import DBSessionMiddleware
 from bot.middlewares.updates_dumper_middleware import UpdatesDumperMiddleware
 from config import get_logging_config, settings
-from database.tables_helper import create_or_drop_db
 from database.database_connector import get_db
 
 
@@ -30,7 +29,6 @@ async def main():
     storage = MemoryStorage()
 
     db = get_db()
-
     dispatcher = Dispatcher(storage=storage)
     db_session_middleware = DBSessionMiddleware(db)
     dispatcher.message.middleware(db_session_middleware)
