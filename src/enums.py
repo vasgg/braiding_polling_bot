0998e24246cd3934f209d710eba0f2293a894b37
vasgg <=>
2024-08-08 03:56:24 +0400

    add nominations enum

--- src/enums.py
+++ src/enums.py
@@ -1,6 +1,17 @@
-from enum import StrEnum, auto
+from enum import IntEnum, StrEnum, auto
 
 
 class Stage(StrEnum):
     PROD = auto()
     DEV = auto()
+
+
+class Nominations(IntEnum):
+    MOST_POPULAR_MASTER = auto()
+    BEST_BRAIDING_MATERIALS_BRAND = auto()
+    BEST_BRAIDING_ACCESSORIES = auto()
+    BEST_BRAIDING_COSMETICS = auto()
+    BRAIDING_QUEEN = auto()
+    BRAIDING_KING = auto()
+    SONG_OF_THE_YEAR = auto()
+    VIDEO_OF_THE_YEAR = auto()
