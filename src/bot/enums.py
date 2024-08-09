e5abdfac1c9c072787fb36013365ce06ae7a2f0c
vasgg <=>
2024-08-09 06:13:40 +0400

    add action enum

--- src/bot/enums.py
+++ src/bot/enums.py
@@ -6,12 +6,17 @@ class Stage(StrEnum):
     DEV = auto()
 
 
-class Nominations(IntEnum):
+class Nomination(IntEnum):
     MOST_POPULAR_MASTER = auto()
     BEST_BRAIDING_MATERIALS_BRAND = auto()
     BEST_BRAIDING_ACCESSORIES = auto()
     BEST_BRAIDING_COSMETICS = auto()
     BRAIDING_QUEEN = auto()
     BRAIDING_KING = auto()
-    SONG_OF_THE_YEAR = auto()
-    VIDEO_OF_THE_YEAR = auto()
+    # SONG_OF_THE_YEAR = auto()
+    # VIDEO_OF_THE_YEAR = auto()
+
+
+class Action(IntEnum):
+    VOTE = auto()
+    BACK = auto()
