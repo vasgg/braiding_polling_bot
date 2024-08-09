8681b2357fc6879a2327038cddedd958ac98b573
vasgg <=>
2024-08-09 06:09:51 +0400

    add some helpers

---
+++ src/bot/internal/helpers.py
@@ -0,0 +1,40 @@
+from bot.enums import Nomination
+from bot.lexicon import texts
+from database.models import Nominee, User
+
+
+def voting_available(user: User) -> bool:
+    return not all(
+        (
+            user.voted_1,
+            user.voted_2,
+            user.voted_3,
+            user.voted_4,
+            user.voted_5,
+            user.voted_6,
+            # user.voted_7,
+            # user.voted_8,
+        )
+    )
+
+
+def number_to_nomination(number: int) -> str:
+    dictionary = {
+        1: 'Самый популярный мастер',
+        2: 'Лучший бренд материала для плетения',
+        3: 'Лучшие украшения и аксессуары для брейдинга',
+        4: 'Лучшая косметика для брейдинга',
+        5: 'Королева брейдинга',
+        6: 'Король брейдинга',
+        # 7: 'Песня года',
+        # 8: 'Видео года',
+    }
+    return dictionary[number]
+
+
+async def get_nominees_list(nomination: Nomination, nominees: list[Nominee]) -> str:
+    text = f'Номинация: <b>{number_to_nomination(nomination.value)}</b>.\n\n'
+    for i, nominee in enumerate(nominees, start=1):
+        name = nominee.name + ' ' + nominee.last_name if nominee.last_name else nominee.name
+        text += texts['nominee_template'].format(i, name, nominee.link)
+    return text
--- src/database/tables_helper.py
+++ src/database/tables_helper.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncEngine
 
-from database_connector import get_db
-from models import Base
+from database_connector import DatabaseConnector, get_db
+from models import Base, Nominee
 
 
 async def create_or_drop_db(engine: AsyncEngine, create: bool = True):
@@ -11,8 +11,20 @@ async def create_or_drop_db(engine: AsyncEngine, create: bool = True):
         else:
             await conn.run_sync(Base.metadata.drop_all)
 
+
+async def populate_db(db_connector: DatabaseConnector):
+    async with db_connector.session_factory.begin() as db_session:
+        for name, lastname, link, nomination in [
+            ('Пётр', 'Семенченко', 'https://youtu.be/8EHKcnoCRJM', 6),
+            ('Данила', 'Дерябин', '-', 6),
+        ]:
+            nominee = Nominee(name=name, last_name=lastname, link=link, nomination=nomination)
+            db_session.add(nominee)
+        await db_session.commit()
+
 if __name__ == '__main__':
     import asyncio
 
     db = get_db()
     asyncio.run(create_or_drop_db(db.engine))
+    # asyncio.run(populate_db(db))
