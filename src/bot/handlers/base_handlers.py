b6855ce60a69cea62765d77827826dc32c323671
vasgg <=>
2024-08-09 19:29:00 +0400

    working on base handlers

--- src/bot/handlers/base_handlers.py
+++ src/bot/handlers/base_handlers.py
@@ -1,5 +1,9 @@
+import contextlib
+
 from aiogram import F, Router, types
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
 from gspread import Client
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -16,17 +20,20 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def start_message(message: types.Message, user: User) -> None:
+async def start_message(message: types.Message, user: User, state: FSMContext) -> None:
+    data = await state.get_data()
+    if data.get('msg_id'):
+        with contextlib.suppress(TelegramBadRequest):
+            await message.bot.delete_message(chat_id=message.chat.id, message_id=data['msg_id'])
     if not user.phone_number:
         await message.answer(text=texts['start_message'])
-        await message.answer(text=texts['without_number_message'],
-                             reply_markup=contact_kb())
+        await message.answer(text=texts['without_number_message'], reply_markup=contact_kb())
         return
     else:
         await message.answer(text=texts['start_message'])
         if voting_available(user):
-            await message.answer(text=texts['nomination_message'],
-                                 reply_markup=nominations_kb(user))
+            msg = await message.answer(text=texts['nomination_message'], reply_markup=nominations_kb(user))
+            await state.update_data(msg_id=msg.message_id)
         else:
             await message.answer(text=texts['no_more_nominations_message'])
 
@@ -35,31 +42,35 @@ async def start_message(message: types.Message, user: User) -> None:
 async def handle_contact(message: types.Message, user: User) -> None:
     if message.contact:
         user.phone_number = message.contact.phone_number
+        await message.answer(text=texts['phone_number_accepted_message'], reply_markup=types.ReplyKeyboardRemove())
         if voting_available(user):
-            await message.answer(text=texts['nomination_message'],
-                                 reply_markup=nominations_kb(user))
+            await message.answer(text=texts['nomination_message'], reply_markup=nominations_kb(user))
 
 
 @router.callback_query(NominationCallback.filter())
-async def handle_nomination(callback: types.CallbackQuery, callback_data: NominationCallback, db_session: AsyncSession) -> None:
+async def handle_nomination(
+    callback: types.CallbackQuery, callback_data: NominationCallback, db_session: AsyncSession
+) -> None:
     await callback.answer()
     nominees: list[Nominee] = await get_nominees(callback_data.nomination, db_session)
-    await callback.message.edit_text(await get_nominees_list(callback_data.nomination, nominees),
-                                     reply_markup=nominee_kb(nominees),
-                                     disable_web_page_preview=True)
+    await callback.message.edit_text(
+        await get_nominees_list(callback_data.nomination, nominees),
+        reply_markup=nominee_kb(nominees),
+        disable_web_page_preview=True,
+    )
 
 
 @router.callback_query(NomineeCallback.filter())
 async def handle_nominee(
-    callback: types.CallbackQuery,
-    callback_data: NomineeCallback,
-    db_session: AsyncSession
+    callback: types.CallbackQuery, callback_data: NomineeCallback, db_session: AsyncSession
 ) -> None:
     await callback.answer()
     nomenee = await get_nominee(callback_data.nominee_id, db_session)
     nomenee_name = nomenee.name + ' ' + nomenee.last_name if nomenee.last_name else nomenee.name
-    await callback.message.edit_text(texts['vote_are_you_sure_message'].format(number_to_nomination(callback_data.nomination), nomenee_name),
-                                     reply_markup=vote_confirmation_kb(callback_data.nomination, callback_data.nominee_id))
+    await callback.message.edit_text(
+        texts['vote_are_you_sure_message'].format(number_to_nomination(callback_data.nomination), nomenee_name),
+        reply_markup=vote_confirmation_kb(callback_data.nomination, callback_data.nominee_id),
+    )
 
 
 @router.callback_query(VoteConfirmationCallback.filter())
@@ -68,7 +79,7 @@ async def handle_vote_confirmation(
     callback_data: NomineeCallback,
     user: User,
     gspread_client: Client,
-    db_session: AsyncSession
+    db_session: AsyncSession,
 ) -> None:
     nominees: list[Nominee] = await get_nominees(callback_data.nomination, db_session)
     if callback_data.action == Action.VOTE:
@@ -84,14 +95,16 @@ async def handle_vote_confirmation(
         await db_session.flush()
         if voting_available(user):
             await callback.answer(texts['vote_accepted_message'], show_alert=True)
-            await callback.message.edit_text(text=texts['nomination_message'],
-                                             reply_markup=nominations_kb(user))
+            await callback.message.edit_text(text=texts['nomination_message'], reply_markup=nominations_kb(user))
             await create_new_vote_record_gsheet(vote, gspread_client, db_session)
             return
         else:
+            await callback.answer(texts['last_vote_accepted_message'], show_alert=True)
             await callback.message.edit_text(texts['no_more_nominations_message'])
             return
     else:
-        await callback.message.edit_text(await get_nominees_list(callback_data.nomination, nominees),
-                                         reply_markup=nominee_kb(nominees),
-                                         disable_web_page_preview=True)
+        await callback.message.edit_text(
+            await get_nominees_list(callback_data.nomination, nominees),
+            reply_markup=nominee_kb(nominees),
+            disable_web_page_preview=True,
+        )
