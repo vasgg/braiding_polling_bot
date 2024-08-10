2faaeb8a14db87fc4ea7c626975d11e3f1586227
vasgg <=>
2024-08-10 13:09:51 +0400

    working on handlers

--- src/bot/handlers/base_handlers.py
+++ src/bot/handlers/base_handlers.py
@@ -1,18 +1,18 @@
+from asyncio import Queue, sleep
 import contextlib
 
 from aiogram import F, Router, types
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
-from gspread import Client
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from bot.enums import Action
-from bot.internal.gsheets import create_new_vote_record_gsheet
+from bot.internal.enums import Action, TaskType
 from bot.internal.helpers import get_nominees_list, number_to_nomination, voting_available
+from bot.internal.schemas import TaskModel, VoteTask
 from bot.keyboards.callbacks import NominationCallback, NomineeCallback, VoteConfirmationCallback
 from bot.keyboards.markups import contact_kb, nominations_kb, nominee_kb, vote_confirmation_kb
-from bot.lexicon import texts
+from bot.internal.lexicon import texts
 from database.crud.nominee import get_nominee, get_nominees
 from database.models import Nominee, User, Vote
 
@@ -27,10 +27,12 @@ async def start_message(message: types.Message, user: User, state: FSMContext) -
             await message.bot.delete_message(chat_id=message.chat.id, message_id=data['msg_id'])
     if not user.phone_number:
         await message.answer(text=texts['start_message'])
+        await sleep(1.5)
         await message.answer(text=texts['without_number_message'], reply_markup=contact_kb())
         return
     else:
         await message.answer(text=texts['start_message'])
+        await sleep(1.5)
         if voting_available(user):
             msg = await message.answer(text=texts['nomination_message'], reply_markup=nominations_kb(user))
             await state.update_data(msg_id=msg.message_id)
@@ -78,10 +80,13 @@ async def handle_vote_confirmation(
     callback: types.CallbackQuery,
     callback_data: NomineeCallback,
     user: User,
-    gspread_client: Client,
+    queue: Queue,
     db_session: AsyncSession,
 ) -> None:
     nominees: list[Nominee] = await get_nominees(callback_data.nomination, db_session)
+    nominee = await get_nominee(callback_data.nominee_id, db_session)
+    username = '@' + user.username if user.username else ''
+    nominee_name = nominee.name + ' ' + nominee.last_name if nominee.last_name else nominee.name
     if callback_data.action == Action.VOTE:
         vote_field_name = f"voted_{callback_data.nomination.value}"
         if hasattr(user, vote_field_name):
@@ -93,10 +98,21 @@ async def handle_vote_confirmation(
         )
         db_session.add(vote)
         await db_session.flush()
+        vote_task: VoteTask = VoteTask(
+            nomination=callback_data.nomination,
+            user_fullname=user.fullname,
+            user_id=user.id,
+            user_username=username,
+            user_phone_number=user.phone_number,
+            nominee_name=nominee_name,
+            user_vote_for=nominee.id,
+            date=vote.created_at.strftime("%d.%m.%Y %H:%M:%S"),
+        )
+        task = TaskModel(task_type=TaskType.VOTE_LOGGER, task_data=vote_task)
+        queue.put_nowait(task)
         if voting_available(user):
             await callback.answer(texts['vote_accepted_message'], show_alert=True)
             await callback.message.edit_text(text=texts['nomination_message'], reply_markup=nominations_kb(user))
-            await create_new_vote_record_gsheet(vote, gspread_client, db_session)
             return
         else:
             await callback.answer(texts['last_vote_accepted_message'], show_alert=True)
